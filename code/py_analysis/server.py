dc4b549d15fb6018381ecf6f834cfbbc616fb155
Jasv1025 <Jasv1025@gmail.com>
2025-06-04 16:53:59 -0600

    refractored server

--- code/py_analysis/server.py
+++ code/py_analysis/server.py
@@ -135,4 +135,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     print(f"[STARTUP] Waitress serving on port {port}...")
     print("[STARTUP] Registered routes:")
-    serve(app, host="0.0.0.0", port=port)
+    serve(host="0.0.0.0", port=port)
